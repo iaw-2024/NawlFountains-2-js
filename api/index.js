54e7aaa115ca906dc87af2964358998beff162f8
Nahuel Fuentes <nahuelfuentes2001@gmail.com>
2024-04-03 12:14:42 -0300

    Implemented DOM variant
    Modified html layout

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ app.get("/express", (req, res) => res.send("Express on Vercel!"));
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 app.use(express.static('public'))
 
-
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
 module.exports = app;
