dacc42a286855c15fa48a691f6c3bb7b2f98422f
Nahuel Fuentes <nahuelfuentes2001@gmail.com>
2024-04-04 17:30:16 -0300

    Rollback fetch url

--- public/cliente_servidor/cliente_servidor_manipulator_script.js
+++ public/cliente_servidor/cliente_servidor_manipulator_script.js
@@ -1,6 +1,6 @@
 function obtenerDatos() {
     ulHTML = document.getElementById("listado_inscripciones");
-    fetch("http://localhost:3001/obtenerDatos")
+    fetch("/obtenerDatos")
     .then(
         response => response.json())
     .then(data => {
