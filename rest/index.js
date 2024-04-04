e3480de138065d0680c9fa92dba0c76f683cbfc7
Nahuel Fuentes <nahuelfuentes2001@gmail.com>
2024-04-04 16:36:08 -0300

    Added port env

--- rest/index.js
+++ rest/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // template for datos.json path
 const datosPath = "./public/datos.json";
