f67489a4a32dcf55141ea81ff0cef9d6eed1d129
Nahuel Fuentes <nahuelfuentes2001@gmail.com>
2024-04-04 23:19:17 -0300

    Debuggin timeout reason

--- api/index.js
+++ api/index.js
@@ -26,23 +26,23 @@ app.get("/express", async (req, res) => {
     let staticHTMLResponse = await fetch(staticHTMLURL);
     let staticHTML = await staticHTMLResponse.text();
 
-    console.log(staticHTML);
-    let dataURL = "http://"+req.hostname+":3001/datos.json";
-    let dataURLResponse = await fetch(dataURL);
-    let data = await dataURLResponse.json();
+    // console.log(staticHTML);
+    // let dataURL = "http://"+req.hostname+":3001/datos.json";
+    // let dataURLResponse = await fetch(dataURL);
+    // let data = await dataURLResponse.json();
 
-    console.log(data);
-    let dynamicHTML = "";
-    let modifiedHTML = "";
+    // console.log(data);
+    // let dynamicHTML = "";
+    // let modifiedHTML = "";
 
-    for (let i = 0; i < data.length; i++) {
-        dynamicHTML += '<li class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 space-y-2 sm:space-y-0 sm:p-3 bg-gray-100 sm:rounded-lg">'+
-        '<p>' + data[i].nombre + '</p>' +
-        '<p>' + data[i].dni + '</p>' +
-        '<p>' + data[i].lu + '</p>' +
-        '<p>' + data[i].email + '</p>' +
-        '<p>' + data[i].fecha + '</p> </li>';
-    }
+    // for (let i = 0; i < data.length; i++) {
+    //     dynamicHTML += '<li class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 space-y-2 sm:space-y-0 sm:p-3 bg-gray-100 sm:rounded-lg">'+
+    //     '<p>' + data[i].nombre + '</p>' +
+    //     '<p>' + data[i].dni + '</p>' +
+    //     '<p>' + data[i].lu + '</p>' +
+    //     '<p>' + data[i].email + '</p>' +
+    //     '<p>' + data[i].fecha + '</p> </li>';
+    // }
     modifiedHTML = staticHTML.replace('</ul>', dynamicHTML + '</ul>');
     res.send(modifiedHTML);
 });
