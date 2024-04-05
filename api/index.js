f6d578839d28baa581119b7b9fe5b33dbd13f6f4
Nahuel Fuentes <nahuelfuentes2001@gmail.com>
2024-04-04 22:58:26 -0300

    Fix pormises sync

--- api/index.js
+++ api/index.js
@@ -23,34 +23,33 @@ app.get("/cliente_servidor", (req, res) => {
 // endpoint that dyanmically generates the list of items in datos.json
 app.get("/express", (req, res) => {
     //Retrive express/listado_tailwindcss.html from public
-    let dataURL = "http://"+req.hostname+"/datos.json";
-    let staticHTMLURL = "http://"+req.hostname+"/express/listado_tailwindcss.html";
+    let dataURL = "http://"+req.hostname+":3001/datos.json";
+    let staticHTMLURL = "http://"+req.hostname+":3001/express/listado_tailwindcss.html";
     let staticHTML = "";
-    
+
     fetch(staticHTMLURL)
     .then(
         response => response.text())
     .then(data => {
         staticHTML = data;
+        fetch (dataURL)
+        .then(
+            response => response.json())
+        .then(data => {
+                let dynamicHTML = "";
+                let modifiedHTML = "";
+                for (let i = 0; i < data.length; i++) {
+                    dynamicHTML += '<li class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 space-y-2 sm:space-y-0 sm:p-3 bg-gray-100 sm:rounded-lg">'+
+                    '<p>' + data[i].nombre + '</p>' +
+                    '<p>' + data[i].dni + '</p>' +
+                    '<p>' + data[i].lu + '</p>' +
+                    '<p>' + data[i].email + '</p>' +
+                    '<p>' + data[i].fecha + '</p> </li>';
+                }
+                modifiedHTML = staticHTML.replace('</ul>', dynamicHTML + '</ul>');
+                res.send(modifiedHTML);
+        })
     });
-
-    fetch (dataURL)
-    .then(
-        response => response.json())
-    .then(data => {
-            let dynamicHTML = "";
-            let modifiedHTML = "";
-            for (let i = 0; i < data.length; i++) {
-                dynamicHTML += '<li class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 space-y-2 sm:space-y-0 sm:p-3 bg-gray-100 sm:rounded-lg">'+
-                '<p>' + data[i].nombre + '</p>' +
-                '<p>' + data[i].dni + '</p>' +
-                '<p>' + data[i].lu + '</p>' +
-                '<p>' + data[i].email + '</p>' +
-                '<p>' + data[i].fecha + '</p> </li>';
-            }
-            modifiedHTML = staticHTML.replace('</ul>', dynamicHTML + '</ul>');
-            res.send(modifiedHTML);
-    })
 });
 
 app.get("/test", (req, res) => {
