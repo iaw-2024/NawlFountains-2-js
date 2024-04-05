f51a1234419a894fcead90f93348d4d6fe1ddfb9
Nahuel Fuentes <nahuelfuentes2001@gmail.com>
2024-04-04 23:12:58 -0300

    Debuging tools

--- api/index.js
+++ api/index.js
@@ -22,17 +22,19 @@ app.get("/cliente_servidor", (req, res) => {
 
 // endpoint that dyanmically generates the list of items in datos.json
 app.get("/express", async (req, res) => {
-    //Retrive express/listado_tailwindcss.html from public
     let staticHTMLURL = "http://"+req.hostname+":3001/express/listado_tailwindcss.html";
     let staticHTMLResponse = await fetch(staticHTMLURL);
     let staticHTML = await staticHTMLResponse.text();
-    
+
+    console.log(staticHTML);
     let dataURL = "http://"+req.hostname+":3001/datos.json";
     let dataURLResponse = await fetch(dataURL);
     let data = await dataURLResponse.json();
 
+    console.log(data);
     let dynamicHTML = "";
     let modifiedHTML = "";
+
     for (let i = 0; i < data.length; i++) {
         dynamicHTML += '<li class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 space-y-2 sm:space-y-0 sm:p-3 bg-gray-100 sm:rounded-lg">'+
         '<p>' + data[i].nombre + '</p>' +
@@ -43,17 +45,6 @@ app.get("/express", async (req, res) => {
     }
     modifiedHTML = staticHTML.replace('</ul>', dynamicHTML + '</ul>');
     res.send(modifiedHTML);
-});
-
-app.get("/test", (req, res) => {
-    let lsFiles = "";
-    fs.readdir(process.cwd()+"/api", (err, files) => {
-        files.forEach(file => {
-            console.log(file);
-          lsFiles += file + "\n";
-        });
-      });
-    res.send(lsFiles);
 });
 
 app.use(express.static('public'))
